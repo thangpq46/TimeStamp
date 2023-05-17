6cbef992ad8f70591cdf15c4e7173fd7049745c6
pqt46 <pqt4621@gmail.com>
2023-05-17 22:10:39 +0700

    a

--- app/src/main/java/com/example/timestamp/MainActivity.kt
+++ app/src/main/java/com/example/timestamp/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.timestamp
 
+import android.app.Application
+import android.content.Context
 import android.content.res.AssetManager
 import android.graphics.RectF
 import android.graphics.Typeface
@@ -20,6 +22,7 @@ import androidx.compose.ui.graphics.ImageBitmapConfig
 import androidx.compose.ui.graphics.Paint
 import androidx.compose.ui.graphics.PaintingStyle
 import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -35,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TestTimeStamp()
                 }
             }
         }
@@ -54,32 +57,44 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     TimeStampTheme {
-        Greeting("Android")
+        TestTimeStamp()
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun TestTimeStamp( ){
+fun TestTimeStamp(){
     val witdh = with(LocalDensity.current){
         300.dp.toPx()
     }.toInt()
     val height = with(LocalDensity.current){
         200.dp.toPx()
     }.toInt()
+    val padding = with(LocalDensity.current){
+        5.dp.toPx()
+    }
+    val asset = LocalContext.current.assets
     val bit = ImageBitmap(witdh, height, config = ImageBitmapConfig.Argb8888)
     val c = androidx.compose.ui.graphics.Canvas(bit)
     c.drawRect(Rect(0f,0f,witdh.toFloat(),height.toFloat()), Paint().apply {
         this.color= Color.Gray
         style= PaintingStyle.Fill
     })
-    val text = "2001.04.06 Mon"
+    val text = "Saturday"
     val p = android.graphics.Paint().apply {
-        textSize=60f
+        textSize=100f
+        typeface= Typeface.createFromAsset(asset,"fonts/aa.ttf")
     }
     val bound = android.graphics.Rect()
     p.getTextBounds(text,0,text.length,bound)
-    c.nativeCanvas.drawText(text,(witdh/2-bound.width()/2f),(height/2+bound.height()/2f),p)
+    val time = "20:57"
+    val ptime = android.graphics.Paint().apply {
+        textSize=80f
+    }
+    val boundTime = android.graphics.Rect()
+    ptime.getTextBounds(time,0,time.length,boundTime)
+    c.nativeCanvas.drawText(text,(witdh/2-bound.width()/2f),(height/2+bound.height()/2f-boundTime.height()),p)
+    c.nativeCanvas.drawText(time,(witdh/2-boundTime.width()/2f),(height/2+bound.height()/2f+padding).toFloat(),ptime)
     Canvas(modifier = Modifier.fillMaxSize(), onDraw ={
         drawImage(bit)
     } )
